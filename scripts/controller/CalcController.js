aea6fa68a5ae4a99860649b8f91db7dd9ee578c7
William Rocha <williamferreira313@gmail.com>
2021-08-18 08:41:48 -0300

    add the dot functiom of the calculator

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -30,7 +30,8 @@ class CalcController {
 
   clearAll() {
     this._operation = [];
-
+    this._lastNumber = "";
+    this._lastOperator = "";
     this.setLastNumberToDisplay();
   }
 
@@ -125,8 +126,6 @@ class CalcController {
     if (isNaN(this.getLastOperation())) {
       if (this.isOperator(value)) {
         this.setLastOperation(value);
-      } else if (isNaN(value)) {
-        console.log(value);
       } else {
         this.pushOperation(value);
         this.setLastNumberToDisplay();
@@ -136,7 +135,7 @@ class CalcController {
         this.pushOperation(value);
       } else {
         let newValue = this.getLastOperation().toString() + value.toString();
-        this.setLastOperation(parseInt(newValue));
+        this.setLastOperation(parseFloat(newValue));
 
         this.setLastNumberToDisplay();
       }
@@ -145,6 +144,16 @@ class CalcController {
 
   setError() {
     this.displayCalc = "ERROR";
+  }
+
+  addDot() {
+    let lastOperation = this.getLastOperation();
+    if (this.isOperator(lastOperation) || !lastOperation) {
+      this.pushOperation("0.");
+    } else {
+      this.setLastOperation(lastOperation.toString() + ".");
+    }
+    this.setLastNumberToDisplay();
   }
 
   execBtn(value) {
@@ -181,7 +190,7 @@ class CalcController {
         this.calc();
         break;
       case "ponto":
-        this.addOperation(".");
+        this.addDot();
 
         break;
       case "0":
